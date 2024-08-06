0c79b10cc9b2e26fe4414c3a32380759813e9980
DavigJ <david.j.jung@gmail.com>
2024-08-06 09:59:31 +0400

    cartwheels can be placed in cardinal directions. fixes #318

--- src/main/java/com/teamabnormals/environmental/common/block/PottedCartwheelBlock.java
+++ src/main/java/com/teamabnormals/environmental/common/block/PottedCartwheelBlock.java
@@ -1,19 +1,28 @@
 package com.teamabnormals.environmental.common.block;
 
+import com.teamabnormals.environmental.core.Environmental;
+import com.teamabnormals.environmental.core.registry.EnvironmentalBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FlowerPotBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Environmental.MOD_ID)
 public class PottedCartwheelBlock extends FlowerPotBlock {
 	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
@@ -40,5 +49,23 @@ public class PottedCartwheelBlock extends FlowerPotBlock {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
+	}
+
+	@SubscribeEvent
+	public static void interact(PlayerInteractEvent.RightClickBlock event) {
+		Level level = event.getLevel();
+		ItemStack stack = event.getItemStack();
+		BlockPos pos = event.getPos();
+		if (level.getBlockState(pos).getBlock() == Blocks.FLOWER_POT && stack.getItem() == EnvironmentalBlocks.CARTWHEEL.get().asItem()) {
+			System.out.println(event.getEntity().getDirection().getOpposite());
+			level.setBlock(pos, EnvironmentalBlocks.POTTED_CARTWHEEL.get().defaultBlockState().setValue(FACING, event.getEntity().getDirection().getOpposite()), 0);
+			event.getEntity().awardStat(Stats.POT_FLOWER);
+			if (!event.getEntity().getAbilities().instabuild) {
+				stack.shrink(1);
+			}
+			event.setUseBlock(Event.Result.DENY);
+			event.setCancellationResult(InteractionResult.SUCCESS);
+			event.setCanceled(true);
+		}
 	}
 }
