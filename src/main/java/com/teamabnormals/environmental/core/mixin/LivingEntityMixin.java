d7562409ddbb81093d42dc85b83f3f6f8e5193cc
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-01-06 01:22:39 -0600

    Closes #426 , #417 , #397

--- src/main/java/com/teamabnormals/environmental/core/mixin/LivingEntityMixin.java
+++ src/main/java/com/teamabnormals/environmental/core/mixin/LivingEntityMixin.java
@@ -3,14 +3,23 @@ package com.teamabnormals.environmental.core.mixin;
 import com.teamabnormals.blueprint.common.world.storage.tracking.IDataManager;
 import com.teamabnormals.environmental.core.other.EnvironmentalDataProcessors;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collection;
+import java.util.Map;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
@@ -25,5 +34,16 @@ public abstract class LivingEntityMixin {
 		}
 
 		return state.getFriction(level, pos, entity);
+	}
+
+	@Shadow
+	@Final
+	private Map<MobEffect, MobEffectInstance> activeEffects;
+
+	@Inject(method = "getActiveEffects", at = @At("HEAD"), cancellable = true)
+	private void getActiveEffects(CallbackInfoReturnable<Collection<MobEffectInstance>> cir) {
+		if (this.activeEffects == null) {
+			cir.setReturnValue(null);
+		}
 	}
 }
